0b6af3a0fb81813f99a222a94226a8776e4180bb
Deane Barker <deane@blendinteractive.com>
2014-11-01 05:47:10 -0500

    Refactored to do the actual import from an IDictionary. Now just need to get any object there.

--- EPiServerChannelLib/EPiServerChannel.cs
+++ EPiServerChannelLib/EPiServerChannel.cs
@@ -1,4 +1,5 @@
-﻿using EPiServerChannelLib.ContentChannel;
+﻿using System.Data;
+using EPiServerChannelLib.ContentChannel;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -53,18 +54,27 @@ namespace EPiServerChannelLib
             var propertyKeys = new ArrayOfString();
             var propertyValues = new ArrayOfString();
 
-            // Reflect all the properties on the object and set them to properties of the same name and value
-            foreach (PropertyInfo propertyDef in obj.GetType().GetProperties())
+            // If it's not the right type, get it there.
+            if (!(obj is IDictionary<string, object>))
             {
-                // If this property has an "Ignore" attribute, then skip it
-                if (propertyDef.GetCustomAttributes(typeof (IgnoreAttribute), false).Any())
+                // DataRow has a specific extension method for this
+                if (obj is DataRow)
                 {
-                    continue;
+                    var row = ((DataRow) obj);
+                    obj = row.Table.Columns.Cast<DataColumn>().ToDictionary(col => col.ColumnName, col => row.Field<object>(col.ColumnName));
                 }
-
-                propertyKeys.Add(propertyDef.Name);
-                propertyValues.Add(Convert.ToString(obj.GetType().GetProperty(propertyDef.Name).GetValue(obj)));
+                else
+                {
+                    obj = ReflectToDictionary(obj);
+                }        
             }
+
+            foreach (var entry in (Dictionary<string, Object>)obj)
+            {
+                propertyKeys.Add(entry.Key);
+                propertyValues.Add(Convert.ToString((entry.Value)));
+            }
+
 
             // If this external key is in the KeyMap, then ensure it has an EPiServer GUID set
             Guid episerverKey = Guid.Empty;
@@ -83,6 +93,25 @@ namespace EPiServerChannelLib
             // Ensure this is valid inside the keymap
             KeyMap.Remove(externalKey);
             KeyMap.Add(externalKey, episerverId);
+        }
+
+        private Dictionary<string, object> ReflectToDictionary(object obj)
+        {
+            var dictionary = new Dictionary<string, object>();
+
+            // Reflect all the properties on the object and set them to properties of the same name and value
+            foreach (PropertyInfo propertyDef in obj.GetType().GetProperties())
+            {
+                // If this property has an "Ignore" attribute, then skip it
+                if (propertyDef.GetCustomAttributes(typeof(IgnoreAttribute), false).Any())
+                {
+                    continue;
+                }
+
+                dictionary.Add(propertyDef.Name, obj.GetType().GetProperty(propertyDef.Name).GetValue(obj));
+            }
+
+            return dictionary;
         }
 
         public int ProcessDeletions()
--- ImportFromCode/Program.cs
+++ ImportFromCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using EPiServerChannelLib;
+﻿using System.Data;
+using EPiServerChannelLib;
 
 namespace ImportFromCode
 {
@@ -8,12 +9,34 @@ namespace ImportFromCode
         {
             var channel = new EPiServerChannel("Deane", "http://beaumontdemo.local/webservices/contentchannelservice.asmx");
 
+            // Import from object
             var page = new Page
             {
                 MainBody = "This is the body",
                 TeaserText = "This is the teaser"
             };
             channel.Process("Deane's Awesome Page", "1234", page);
+
+            // Import from datarow
+            var table = new DataTable();
+            var mainBody = new DataColumn()
+            {
+                DataType = typeof (string),
+                ColumnName = "MainBody"
+            };
+            var teaserText = new DataColumn()
+            {
+                DataType = typeof(string),
+                ColumnName = "TeaserText"
+            };
+            table.Columns.Add(mainBody);
+            table.Columns.Add(teaserText);
+            var row = table.NewRow();
+
+            row["MainBody"] = "This is the body";
+            row["TeaserText"] = "This is the teaser text";
+
+            channel.Process("Imported from Data Row", "imported-from-data-row", row);
 
             channel.Close();
         }
